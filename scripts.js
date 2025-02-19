3e4708a223098a932a4374cf6df534d85255aee1
Carolyne Lira <carol017lira@gmail.com>
2025-02-19 16:06:05 -0300

    feat: Cria item de lista de solicitações
    Adiciona evento de submit ao formulário e captura
    valores do formulário
    Cria função que adiciona item de despesa na lista
    de solicitação

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+const expenseList = document.querySelector("ul")
+
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
@@ -15,4 +21,47 @@ function formatCurrencyBRL(value) {
   })
 
   return value
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    expenseInfo.append(expenseName, expenseCategory)
+    expenseItem.append(expenseIcon, expenseInfo)
+    expenseList.append(expenseItem)
+
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.")
+    console.log(error)
+  }
 }
